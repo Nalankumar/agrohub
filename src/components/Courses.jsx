199146791cacd3bb09693a44c35fbdde372045e3
Nalankumar <Nalankumar@users.noreply.github.com>
2024-11-01 20:13:55 +0530

    responsiveness added

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,9 +1,9 @@
 import Carousal from './Carousal'
 export default function Courses(){
 	return(
-		<div className="hero bg-base-200 min-h-screen min-w-screen">
-		  <div className="hero-content flex-col">
-		  	<h1>Topics</h1>
+		<div className="hero bg-base-200 min-h-screen">
+		  <div className="hero-content flex-col lg:flex-row">
+		  	<h1 className="text-3xl font-bold mx-2">Topics</h1>
 		    <Carousal />
 		  </div>
 		</div>
