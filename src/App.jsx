38fa3221d9da2dd0073ab2e825358f248f6fbd38
Nalankumar <Nalankumar@users.noreply.github.com>
2024-11-05 21:16:25 +0530

    Footer added to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './components/NavBar/Navbar'
 import HeroPage from './components/HeroPage'
 import Courses from './components/Courses'
+import Footer from './components/Footer'
 function App() {
   return (
     <div>
       <Navbar />
       <HeroPage />
       <Courses />
+      <Footer />
     </div>
   )
 }
